2c1caa1998149cc47b02fd467f9d03a44c9d004c
Francesco <shoe.francesco@gmail.com>
2019-12-11 00:48:58 +0000

    ADD: update db dump

--- db.sql
+++ db.sql
@@ -2,8 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 10.7 (Debian 10.7-1.pgdg90+1)
--- Dumped by pg_dump version 12.1 (Ubuntu 12.1-1.pgdg18.04+1)
+-- Dumped from database version 11.6 (Ubuntu 11.6-1.pgdg19.10+1)
+-- Dumped by pg_dump version 11.6 (Ubuntu 11.6-1.pgdg19.10+1)
+
+-- Started on 2019-12-11 00:46:28 GMT
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,6 +19,110 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- TOC entry 3097 (class 1262 OID 16384)
+-- Name: simple-notes; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE "simple-notes" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_GB.UTF-8' LC_CTYPE = 'en_GB.UTF-8';
+
+
+ALTER DATABASE "simple-notes" OWNER TO postgres;
+
+\connect -reuse-previous=on "dbname='simple-notes'"
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 199 (class 1259 OID 16417)
+-- Name: courses_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.courses_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.courses_id_seq OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 198 (class 1259 OID 16404)
+-- Name: courses; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.courses (
+    description character varying(256),
+    id bigint DEFAULT nextval('public.courses_id_seq'::regclass) NOT NULL,
+    name character varying(256),
+    "userId" bigint NOT NULL
+);
+
+
+ALTER TABLE public.courses OWNER TO postgres;
+
+--
+-- TOC entry 200 (class 1259 OID 16420)
+-- Name: lessons_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.lessons_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.lessons_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 201 (class 1259 OID 16422)
+-- Name: lessons; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.lessons (
+    id bigint DEFAULT nextval('public.lessons_id_seq'::regclass) NOT NULL,
+    name character varying,
+    description character varying,
+    "courseId" bigint,
+    "userId" bigint
+);
+
+
+ALTER TABLE public.lessons OWNER TO postgres;
+
+--
+-- TOC entry 202 (class 1259 OID 16440)
+-- Name: notes_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.notes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.notes_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 196 (class 1259 OID 16385)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -30,9 +136,8 @@ CREATE SEQUENCE public.users_id_seq
 
 ALTER TABLE public.users_id_seq OWNER TO postgres;
 
-SET default_tablespace = '';
-
 --
+-- TOC entry 197 (class 1259 OID 16387)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -48,49 +153,122 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
+-- TOC entry 3087 (class 0 OID 16404)
+-- Dependencies: 198
+-- Data for Name: courses; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 3090 (class 0 OID 16422)
+-- Dependencies: 201
+-- Data for Name: lessons; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 3086 (class 0 OID 16387)
+-- Dependencies: 197
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.users (email, id, nickname, password, created) FROM stdin;
-asd@asd.asd	1	asd	$2y$10$gWFH47mMXhTQ4m.5Egh.Y.W78YgeE/ARrSX.vCeQBQfu5kEsw1WZe	\N
-asd@asd.asd	2	asd	$2y$10$4zHHQtjqhqlfueYWRsxnZ.xjHJkmJOyeo9ebcUrAhk9jFffG1jrx6	\N
-asd@asd.asd	3	asd	$2y$10$GOyMdAI0fUgYeRQHfKUu2OOrmYrZbp5MtVBRh77aOPf7pmDz1FrDW	\N
-asd@asd.asd	4	asd	$2y$10$LqBBQlHXc9vReFioVgxez.fNI3ty1rBCCqxvFpIGhRVpYQQheVWa.	\N
-asd@asd.asd	5	asd	$2y$10$BOgnooaXuXTI7jXtuRYHNeUs/vgbU4cWCtuPYXEaZwSKRsxOVjnZW	\N
-asd@asd.asd	6	asd	$2y$10$q.cpbmOg9L7hWqq7lGrYk.B6VLbhwi5bRj9R0udpZ.eoAd9.GB3OG	\N
-asd@asd.asd	7	asd	$2y$10$17lRjVUMjd8h3U5HmydBJOY6edz0F0pgwRBvfY6SRnQ9/DtzT5c8a	\N
-asd@asd.asd	8	asd	$2y$10$HbeUou/bNFRxmwy7dUlWA.WHe06hZ.blC5GkiJXqKh3Fnp2XgvLCO	\N
-asd@asd.asd	9	asd	$2y$10$vlFLSefhtbHqwj24fuJPcunpARCll5BWGArn/mkO08oN2h9DGcCae	\N
-asd@asd.asd	10	asd	$2y$10$rh3KVkDtrBJ45bra2a1iF.CVW.E6DtvBvS1wR5HOi.aArhlV/rCaO	\N
-asd@asd.asd	11	asd	$2y$10$K5E/eaAs.RQRFrUsz1IKF.a4Rhf6/iOMVsmxyhacIkCtkMO152IlW	\N
-asd@asd.asd	12	asd	$2y$10$VOuJsf3sRdbGJ5NwzOvymeHQrk0/n8Rulp399NcFDfntLJL8BCLom	\N
-asd@asd.asd	13	asd	$2y$10$4uof8wF9vlRdeV0iZRGPUOZvYib/FGJWjJwRoB34wqT4sE/xHpzja	\N
-asd@asd.asd	14	asd	$2y$10$HuYogMrlhXIfWqcd6KZzWefn14NG9cdpjvs6cYlOHv5/iq/C/OwdW	\N
-asd@asd.asd	15	asd	$2y$10$Y/5PbWClJlDGq.IITID4.uttcnxwSQY/ksZ9YzOJd6qIX0bPg6/Ce	\N
-asd@asd.asd	16	asd	$2y$10$D.kBsdYPLDmoUnQo9zX5ZOPHYCWA14YteMB/579r2r2Uv.ubY.eiu	\N
-asd@asd.asd	17	asd	$2y$10$pntZTu7cKmyO1IAH00irZ.387zJHHAXPUAc4oM7PC3mpHWQcwpKvO	\N
-asd@asd.asd	18	asd	$2y$10$vLZPuGXwPSL58vkwg.jlJeA/ATIRQH084LqOlAHuMrfHmtntnEFn.	\N
-asd@asd.asd	19	asd	$2y$10$U4bIaq4xTsqqD6tQUL/STe/jcRCpHcXT7x8lKDDq.cmGJNgQ4Pfb2	\N
-asd@asd.asd	20	asd	$2y$10$y5N7Tl9P2qhlBIG6z/kgJeeLPbqod82bgzE7gsGj31M8tmqnu6FUu	\N
-asd@asd.asd	21	asd	$2y$10$28whdhTVjGdFcFPleWur/OZIiiN56aAwJp04pKm9nB8J2YhBOYpii	\N
-asd@asd.asd	22	asd	$2y$10$G/vZcnGw2YTBua9zPPnuoeBHWfLWeXxZIvEW3jIDjfkKhHJAYi9ju	\N
-\.
+INSERT INTO public.users (email, id, nickname, password, created) VALUES ('asd@asd.com', 28, 'aldo', '$2y$10$DdEIgqr9ckq6kfMPyBLBcuPjs8SEOu.fkM3HwUDsq4CSiXvDaFoci', NULL);
+INSERT INTO public.users (email, id, nickname, password, created) VALUES ('user@user.com', 29, 'user1', '$2y$10$U7FanT.V8XE1rU5urtnKzu4pBh26rcHOdiFuvqbbB0OCJQWve6gFG', NULL);
 
 
 --
+-- TOC entry 3098 (class 0 OID 0)
+-- Dependencies: 199
+-- Name: courses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.courses_id_seq', 1, false);
+
+
+--
+-- TOC entry 3099 (class 0 OID 0)
+-- Dependencies: 200
+-- Name: lessons_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.lessons_id_seq', 1, false);
+
+
+--
+-- TOC entry 3100 (class 0 OID 0)
+-- Dependencies: 202
+-- Name: notes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.notes_id_seq', 1, false);
+
+
+--
+-- TOC entry 3101 (class 0 OID 0)
+-- Dependencies: 196
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 22, true);
+SELECT pg_catalog.setval('public.users_id_seq', 29, true);
 
 
 --
+-- TOC entry 2958 (class 2606 OID 16411)
+-- Name: courses courses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.courses
+    ADD CONSTRAINT courses_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2960 (class 2606 OID 16429)
+-- Name: lessons lessons_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.lessons
+    ADD CONSTRAINT lessons_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2956 (class 2606 OID 16395)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
+
+--
+-- TOC entry 2963 (class 2606 OID 16435)
+-- Name: lessons courseId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.lessons
+    ADD CONSTRAINT "courseId" FOREIGN KEY ("courseId") REFERENCES public.courses(id) NOT VALID;
+
+
+--
+-- TOC entry 2961 (class 2606 OID 16412)
+-- Name: courses userId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.courses
+    ADD CONSTRAINT "userId" FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
+--
+-- TOC entry 2962 (class 2606 OID 16430)
+-- Name: lessons userId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.lessons
+    ADD CONSTRAINT "userId" FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
+-- Completed on 2019-12-11 00:46:28 GMT
 
 --
 -- PostgreSQL database dump complete
